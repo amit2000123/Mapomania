05f115787e03d5e36f507e6a3b4f9f30361dcf9b
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-10-20 10:00:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
-let cityRef = document.getElementById("city");
-
-//Function to fetch weather details from api and display them
-let getWeather = () => {
-  let cityValue = cityRef.value;
-  //If input field is empty
-  if (cityValue.length == 0) {
-    result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
-  }
-  //If input field is NOT empty
-  else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
-    //Clear the input field
-    cityRef.value = "";
-    fetch(url)
-      .then((resp) => resp.json())
-      //If city name is valid
-      .then((data) => {
-        console.log(data);
-        console.log(data.weather[0].icon);
-        console.log(data.weather[0].main);
-        console.log(data.weather[0].description);
-        console.log(data.name);
-        console.log(data.main.temp_min);
-        console.log(data.main.temp_max);
-        result.innerHTML = `
-        <h2>${data.name}</h2>
-        <h4 class="weather">${data.weather[0].main}</h4>
-        <h4 class="desc">${data.weather[0].description}</h4>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1>${data.main.temp} &#176;</h1>
-        <div class="temp-container">
-            <div>
-                <h4 class="title">min</h4>
-                <h4 class="temp">${data.main.temp_min}&#176;</h4>
-            </div>
-            <div>
-                <h4 class="title">max</h4>
-                <h4 class="temp">${data.main.temp_max}&#176;</h4>
+let countryInp = document.getElementById("country-inp");
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      //   console.log(data[0]);
+      //   console.log(data[0].capital[0]);
+      //   console.log(data[0].flags.svg);
+      //   console.log(data[0].name.common);
+      //   console.log(data[0].continents[0]);
+      //   console.log(Object.keys(data[0].currencies)[0]);
+      //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+      //   console.log(
+      //     Object.values(data[0].languages).toString().split(",").join(", ")
+      //   );
+      result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
             </div>
         </div>
-        `;
-      })
-      //If city name is NOT valid
-      .catch(() => {
-        result.innerHTML = `<h3 class="msg">City not found</h3>`;
-      });
-  }
-};
-searchBtn.addEventListener("click", getWeather);
-window.addEventListener("load", getWeather);
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
+        </div>
+      `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+});
